fbf35e68c469ce5c61923e641a0e3f6abb3cdb65
Hilary Egan <hilaryye@gmail.com>
2024-05-20 14:58:03 -0600

    Adding custom workflow example

---
+++ examples/custom_mf_workflow.py
@@ -0,0 +1,50 @@
+from adaptive_computing.datasets import ContinuousVariable
+from adaptive_computing.drivers import ActiveLoopDriverMF
+import numpy as np
+
+def func_lf(x):
+    return (x-3)**2
+
+def func_hf(x):
+    return (x-3)**2+0.1*np.sin(x)
+
+class CustomMFDriver(ActiveLoopDriverMF):
+
+    def step(self):
+        """
+        Designing a workflow that always runs all lower
+        fidelity models if sampling a higher fidelity model point.
+        """
+        x_samples = np.zeros((self.n_fl, 1, self.dataset.n_in))
+        objs = np.zeros(self.n_fl)
+        for f_i in range(self.n_fl):
+            x = self.sampler.get_sample(self.surrogate, self.dataset, f_i)
+            y = self.sampler.acq_func(x, self.surrogate, self.dataset, f_i)
+
+            x_samples[f_i] = x
+            objs[f_i] = y
+
+        fi_eval = np.argmin(objs/self.fidelity_costs)
+        x = x_samples[fi_eval]
+
+        for f_i in range(0, fi_eval):
+            y = self.evaluate_sample(x, f_i)
+
+            self.dataset.add_samples(x,y, n_fidelity=f_i)
+
+def example_bayesian_1d_sf():
+
+    params = [ContinuousVariable(min=0, max=10)]
+
+    ac_driver = CustomMFDriver(simulations=[func_lf,
+                                            func_hf],
+                                   fidelity_costs=[1,10],
+                                   params=params,
+                                   surrogate='SMT')
+    
+    ac_driver.run(N_steps = 10)
+
+
+
+if __name__ == "__main__":
+    example_bayesian_1d_sf()
