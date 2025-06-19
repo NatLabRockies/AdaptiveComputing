557f166b2b12f5fbf4c8cf0e9906bcd2593f5c8f
Kevin Griffin <kgriffin@nrel.gov>
2025-06-18 20:32:40 -0600

    Online inference example code also uses autonomous managers.

--- examples/hero_2clusters/controller_online_inference.py
+++ examples/hero_2clusters/controller_online_inference.py
@@ -12,10 +12,34 @@ def print_data(ac_driver):
     print(f"y_data = {ac_driver.dataset.y_data[0]}")
     return
 
+# Notes:
+# How to manually attach to the session: tmux attach-session -t manager_session
+# How to manually terminate the session: tmux kill-session -t manager_session
+
 if __name__ == '__main__':
+    # for testing, use two managers on vermilion on different login nodes
+    machine_names = ['vermilion1','vermilion2']
+    remote_usernames = {'vermilion1':'kgriffin','vermilion2':'kgriffin'}
+    remote_hosts = {'vermilion1':'vs-login-1.hpc.nrel.gov','vermilion2':'vs-login-2.hpc.nrel.gov'} # Note: make sure to specify a specific login node, otherwise it is unlikely you can reattach to a previously started tmux session when cleaning up
+    remote_dirs = {'vermilion1':'/projects/degrees/kgriffin/AdaptiveComputing/examples/hero_2clusters/','vermilion2':'/projects/degrees/kgriffin/AdaptiveComputing/examples/hero_2clusters/'}
+
+    # # machine_names = ['kestrel','vermilion']
+    # machine_names = ['kestrel']
+    # # machine_names = ['vermilion']
+    # remote_usernames = {'kestrel':'kgriffin','vermilion':'kgriffin'}
+    # remote_hosts = {'kestrel':'kl1.hpc.nrel.gov','vermilion':'vs-login-1.hpc.nrel.gov'} # Note: make sure to specify a specific login node, otherwise it is unlikely you can reattach to a previously started tmux session when cleaning up
+    # remote_dirs = {'kestrel':'/home/kgriffin/AdaptiveComputing_1.0/AdaptiveComputing/examples/hero_2clusters/','vermilion':'/projects/degrees/kgriffin/AdaptiveComputing/examples/hero_2clusters/'}
+
+    from autonomous_managers import run_remote_managers, cleanup_remote_managers, setup_remote_state
+    # register a signal handler and set up the variables it needs to operate
+    setup_remote_state(machine_names, remote_usernames, remote_hosts, remote_dirs)
+    run_remote_managers()
+    
     # Unpickle the offline trained surrogate (created by controller_offline_training.py)                                                                               
     with open('offline_training.pkl', 'rb') as file:
         ac_driver = pickle.load(file)
+
+    assert(ac_driver.dataset.machine_names == machine_names)
 
     ac_driver.dataset.hero_authenticate()
 
@@ -58,6 +82,8 @@ if __name__ == '__main__':
     # expect that the second time, no simulations are launched and the outputs are the same
 
     ac_driver.hero_wait_for_data_and_train()
+
+    cleanup_remote_managers()
 
     # Save the driver to a pickle file                                                                     
     with open('online_training.pkl', 'wb') as file:
